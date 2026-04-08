5404042c04d941bed53e522f7800ebad3a73995a
mussammilshathik21 <mussammilshathik2@gmail.com>
2026-04-08 16:17:42 +0530

    switch database to sqlite

--- config/settings.py
+++ config/settings.py
@@ -87,12 +87,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecommerce_db',
-        'USER': 'root',
-        'PASSWORD': 'codex',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
