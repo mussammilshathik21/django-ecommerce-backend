a84a7b78dad7be584544e0569c89880c60b4f0d7
mussammilshathik21 <mussammilshathik2@gmail.com>
2026-04-08 18:12:08 +0530

    remove admin auto creation

--- config/settings.py
+++ config/settings.py
@@ -171,13 +171,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = "/media/"
 
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        "admin",
-        "admin@email.com",
-        "admin123"
-    )
